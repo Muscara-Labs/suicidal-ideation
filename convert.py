150df1a300d6ab9d77a1d7db75702f637f1a2504
Kevin Muscara <muscarak1@nku.edu>
2023-09-25 13:48:17 -0400

    api for prediction testing

---
+++ convert.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+model = tf.keras.models.load_model('./suicide_classifier')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+  input_text = request.args.get('text', '')
+  if not input_text:
+    return jsonify({'error': 'Please provide input text as a parameter.'}), 400
+  
+  prediction = model.predict([input_text])
+  
+  return jsonify(prediction.tolist()[0][0] * 100), 200
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=801)
